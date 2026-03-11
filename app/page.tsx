273ba91cbbce009dc677c602e0ee54d42af90a5c
Devanshu <devanshup.pandey281@gmail.com>
2026-03-11 13:19:25 +0530

    minor UI fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import Pricing from "@/pages/Pricing";
 export default function Home() {
   return (
     <div
-      className="min-h-screen bg-linear-180-to-br from-orange-500/20 via-transparent to-[#BF35FF]
-      bg-[radial-gradient(ellipse_50%_50%_at_50%_30%,#FF5F2D40_0%,#060B1320_100%),radial-gradient(ellipse_50%_50%_at_50%_80%,#FF5F2D40_0%,#060B1320_100%)]"
+      className="min-h-dvh bg-[#060B13] bg-linear-180-to-br from-orange-500/20 via-transparent to-[#BF35FF]
+      bg-[radial-gradient(ellipse_50%_10%_at_90%_18%,#8625B350_0%,#060B1340_100%)]
+      md:bg-[radial-gradient(ellipse_50%_50%_at_50%_30%,#FF5F2D40_0%,#060B1320_100%),radial-gradient(ellipse_50%_50%_at_50%_80%,#FF5F2D40_0%,#060B1320_100%)]"
     >
       <Navbar />
       <main className="max-w-6xl mx-auto px-4 lg:px-0">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import BrandLogo from "./BrandLogo";
 
@@ -7,8 +7,20 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isMenuOpen]);
 
   return (
     <>
@@ -35,6 +47,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+
         <Button>Start Free Trial</Button>
 
         {/* Mobile Hamburger */}
@@ -59,41 +72,40 @@ const Navbar = () => {
 
       {/* Mobile Fullscreen Menu */}
       {isMenuOpen && (
-        <div className="lg:hidden bg-white fixed top-0 left-0 w-full h-full p-4">
-          <div
-            className="flex items-center justify-between"
-            onClick={(e) => e.stopPropagation()}
-          >
+        <div className="lg:hidden bg-white fixed inset-0 p-4 z-50 min-h-screen">
+          <div className="flex items-center justify-between">
             <BrandLogo variant="dark" />
-            <button onClick={toggleMenu} className="">
+
+            <button onClick={toggleMenu}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="24px"
+                height="24"
                 viewBox="0 -960 960 960"
-                width="24px"
-                fill="#000000"
+                width="24"
+                fill="#000"
               >
                 <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
               </svg>
             </button>
           </div>
 
-          <div className="flex flex-col items-center gap-4 w-ful mt-10 text-black/80">
-            <Button>Start Free Trail</Button>
+          <div className="flex flex-col items-center gap-4 w-full mt-10 text-black/80">
+            <Button>Start Free Trial</Button>
             <p className="text-lg font-medium">Sign in</p>
 
             {["Features", "Pricing", "Use cases"].map((item, index) => (
               <li
                 key={index}
-                className="w-full text-left py-4 px-6 bg-light-shade text-lg flex items-center justify-between rounded-md "
+                className="w-full text-left py-4 px-6 bg-light-shade text-lg flex items-center justify-between rounded-md list-none"
               >
                 {item}
+
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  height="12px"
+                  height="12"
                   viewBox="0 -960 960 960"
-                  width="12px"
-                  fill="#000000"
+                  width="12"
+                  fill="#000"
                 >
                   <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
                 </svg>
